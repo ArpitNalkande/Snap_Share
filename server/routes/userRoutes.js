a5b8767609b76c944267c758065de68048edcf7e
ArpitNalkande <arpitn1603@gmail.com>
2024-02-07 23:06:41 -0800

    new commit

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,7 +6,7 @@ import {
   requestPasswordReset,
   resetPassword,
 } from "../controllers/userController.js";
-// import userAuth from "../middleware/authMiddleware.js";
+import userAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 const __dirname = path.resolve(path.dirname(""));
@@ -16,6 +16,10 @@ router.get("/verify/:userId/:token", verifyEmail);
 router.post("/request-passwordreset", requestPasswordReset);
 router.get("/reset-password/:userId/:token", resetPassword);
 router.post("/reset-password", changePassword);
+
+// user routes
+router.post("/get-user/:id?", userAuth, getUser);
+router.put("/update-user", userAuth, updateUser);
 
 router.get("/verified", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/build", "index.html"));
